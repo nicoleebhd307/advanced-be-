0b78331547c3d6b5bacf30a1661c0027f37b830d
Hoang Dieu <dieubh234112e@st.uel.edu.vn>
2026-01-19 00:20:52 +0700

    exercise10

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,6 +1,7 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
@@ -12,6 +13,7 @@ import { Customer18 } from './customer18/customer18';
 
 import { ListCustomer } from './list-customer/list-customer';
 import { Product14 } from './product14/product14';
+import { Exercise10 } from './exercise10/exercise10';
 
 @NgModule({
   declarations: [
@@ -24,12 +26,14 @@ import { Product14 } from './product14/product14';
     Customer18,
 
     ListCustomer,
-    Product14
+    Product14,
+    Exercise10
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule   // for making HTTP requests: dữ liệu nằm ở bên ngoài (BE)
+    HttpClientModule,   // for making HTTP requests: dữ liệu nằm ở bên ngoài (BE)
+    FormsModule        // for two-way binding with [(ngModel)]
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
